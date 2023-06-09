e07ce3832e1b056f0922508adc9aefb3ef4290fa
shanjb0221 <shanjb20@mails.tsinghua.edu.cn>
2023-06-10 00:52:09 +0800

    tidy(duck): remove 32-bit

--- kernel/duck/kern/trap.cpp
+++ kernel/duck/kern/trap.cpp
@@ -98,11 +98,9 @@ static char interrupt_stack[8][STACK_SIZE] __attribute__((aligned(4096)));
 
 const uint8_t TRAP_IRQ = 32;
 const uint8_t TRAP_RUN_USER = 233;
-const uint8_t TRAP_RUN_USER32 = 234;
 const uint8_t TRAP_ABORT = 253;
 const uint8_t TRAP_INVALID_SYSCALL = 254;
 const uint8_t TRAP_SYSCALL = 255;
-const uint8_t TRAP_SYSCALL32 = 128;
 
 extern "C" {
 uint64_t __syscall_stack;
@@ -113,13 +111,6 @@ static uint64_t user_time_limit_ns;
 extern "C" void trap_return(Trapframe *tf);
 
 extern "C" void syscall_return_record_tsc(Trapframe *tf);
-
-extern "C" void syscall_return_32_record_tsc(Trapframe *tf);
-
-extern "C" {
-void __trap_0x80_entry();
-void __trap_0x80_return(Trapframe *tf);
-}
 
 static void set_idt(int id, void *addr) {
     uint64_t a = (uint64_t)addr;
@@ -159,8 +150,8 @@ extern "C" void trap_handler(Trapframe *tf) {
     LDEBUG("trap %d, CPL %d", (int)num, (int)(tf->tf_cs & 3));
 
     if (tf->tf_cs & 3) { // trap from user
-        LAPIC::eoi();
-        LAPIC::timer_disable();
+        // LAPIC::eoi();
+        // LAPIC::timer_disable();
         tf_from_user = *tf;
         tf = &tf_run_user;
         tf->tf_regs.rax = num;
@@ -188,10 +179,10 @@ extern "C" void trap_handler(Trapframe *tf) {
         if (num == TRAP_IRQ + PIC::IRQ_TIMER) {
             // timer interrupt caused by scheduler
             // just return with interrupts disabled
-            LAPIC::eoi();
-            LAPIC::timer_disable();
+            // LAPIC::eoi();
+            // LAPIC::timer_disable();
             tf->tf_rflags &= ~0x200; // disable interrupts
-        } else if (num == TRAP_RUN_USER || num == TRAP_RUN_USER32) {
+        } else if (num == TRAP_RUN_USER) {
             tf_run_user = *tf;
 
             // clear gdt tls
@@ -200,44 +191,26 @@ extern "C" void trap_handler(Trapframe *tf) {
 
             using x86_64::rdmsr;
             using x86_64::wrmsr;
-            if (num == TRAP_RUN_USER32) {
-                // set up segment registers
-                __asm__ volatile("mov %0, %%ax" : : "i"(GDT_USER32_SS | 3));
-                __asm__ volatile("mov %ax, %ds");
-                __asm__ volatile("mov %ax, %es");
-                __asm__ volatile("mov %ax, %fs");
-                __asm__ volatile("mov %ax, %gs");
-                __asm__ volatile("" : : : "%ax");
 
-                // enable int 0x80
-                set_idt(0x80, (void *)&__trap_0x80_entry);
-                idt[0x80].options |= 3 << 5;
-                x86_64::lidt(&idt_desc);
-            } else { // TRAP_RUN_USER
-                // clear segment registers
-                __asm__ volatile("mov $0, %ax");
-                __asm__ volatile("mov %ax, %ds");
-                __asm__ volatile("mov %ax, %es");
-                __asm__ volatile("mov %ax, %fs");
-                __asm__ volatile("mov %ax, %gs");
-                __asm__ volatile("" : : : "%ax");
+            // clear segment registers
+            __asm__ volatile("mov $0, %ax");
+            __asm__ volatile("mov %ax, %ds");
+            __asm__ volatile("mov %ax, %es");
+            __asm__ volatile("mov %ax, %fs");
+            __asm__ volatile("mov %ax, %gs");
+            __asm__ volatile("" : : : "%ax");
 
-                // disable int 0x80
-                set_idt(0x80, __traps[0x80]);
-                x86_64::lidt(&idt_desc);
-            }
+            // disable int 0x80
+            set_idt(0x80, __traps[0x80]);
+            x86_64::lidt(&idt_desc);
 
             if (user_time_limit_ns != 0) {
                 // 0.5ms per interrupt, add 10ms + 0.1% for context switching
-                uint64_t timer_cnt = user_time_limit_ns / 500000ul;
-                LAPIC::timer_periodic_ns(500000, timer_cnt + timer_cnt / 1000ul + 20);
+                // uint64_t timer_cnt = user_time_limit_ns / 500000ul;
+                // LAPIC::timer_periodic_ns(500000, timer_cnt + timer_cnt / 1000ul + 20);
             }
 
-            if (num == TRAP_RUN_USER) {
-                syscall_return_record_tsc(&tf_to_user);
-            } else { // TRAP_RUN_USER_32
-                syscall_return_32_record_tsc(&tf_to_user);
-            }
+            syscall_return_record_tsc(&tf_to_user);
         } else {
             uint64_t cr2;
             __asm__ volatile("movq %%cr2, %0" : "=r"(cr2));
@@ -251,75 +224,6 @@ extern "C" void trap_handler(Trapframe *tf) {
     }
 
     trap_return(tf);
-}
-
-extern "C" {
-uint64_t __espfix_new_kernel_rsp;
-}
-
-// orig_rsp: rsp when iret
-// new_rsp: tf_rsp
-static void prepare_espfix(uint64_t orig_rsp, uint64_t new_rsp) {
-    uint64_t base_addr = ((orig_rsp >> 16) - (new_rsp >> 16)) << 16;
-    base_addr = (uint64_t)(uint32_t)base_addr;
-    uint64_t tmp = 0xffff;
-    tmp |= (base_addr & 0xffffff) << 16;
-    tmp |= (base_addr >> 24) << 56;
-    tmp |= 0x00cf920000000000; // 32-bit, writable, 4k-granularity, kernel
-    gdt[GDT_ESPFIX_SS >> 3] = tmp;
-    __espfix_new_kernel_rsp = (uint32_t)(orig_rsp - base_addr);
-}
-
-extern "C" void trap_0x80_handler(Trapframe *tf) {
-    // must from 32-bit userspace
-    int syscall_num = (int)(uint32_t)tf->tf_regs.rax;
-
-    if (syscall_num == ABI::DUCK_sys_set_thread_area) {
-        uint64_t rbx = (uint64_t)(uint32_t)tf->tf_regs.rbx;
-        uint64_t ret = -EINVAL;
-
-        // prepare espfix
-        prepare_espfix((uint64_t)&tf->tf_rip, tf->tf_rsp);
-
-        using Memory::user_writable_check;
-        if (!user_writable_check(rbx)) {
-            ret = -EFAULT;
-        } else if (!user_writable_check(rbx + sizeof(user_desc) - 1)) {
-            ret = -EFAULT;
-        } else {
-            user_desc *desc = (user_desc *)rbx;
-            if (desc->entry_number != -1u && desc->entry_number != GDT_USER32_TLS / 8) {
-                ret = -EINVAL;
-            } else {
-                uint64_t tmp = 0;
-                tmp = desc->limit & 0xffff;
-                tmp |= (((uint64_t)desc->base_addr) & 0xffffff) << 16;
-                tmp |= (uint64_t)(desc->base_addr >> 24) << 56;
-                // TODO FIXME: Check other flags in user_desc
-                tmp |= 0x00cff20000000000; // 32-bit, writable, 4k-granularity
-                gdt[GDT_USER32_TLS >> 3] = tmp;
-                x86_64::lgdt(&gdt_desc);
-
-                desc->entry_number = GDT_USER32_TLS >> 3;
-                ret = 0;
-            }
-        }
-
-        if (ret != 0) {
-            // apply espfix
-            x86_64::lgdt(&gdt_desc);
-        }
-
-        // return value
-        tf->tf_regs.rax = ret;
-
-        // fix userspace ss since it could be invalid
-        tf->tf_ss = GDT_USER32_SS | 3;
-
-        __trap_0x80_return(tf);
-    } else {
-        trap_handler(tf);
-    }
 }
 
 extern "C" void __syscall_entry();
@@ -346,10 +250,10 @@ void init() {
     task_state.IOPB_offset = sizeof(TaskState);
 
     // fix TSS
-    uintptr_t task_state_phys = virt2phys((uintptr_t)&task_state);
-    gdt[GDT_TSS >> 3] = (sizeof(TaskState) - 1) | ((task_state_phys & 0xffffff) << 16) | (9ul << 40) | (0ul << 45) |
-                        (1ul << 47) | (1ul << 53) | ((task_state_phys >> 24) << 56);
-    gdt[(GDT_TSS >> 3) + 1] = (task_state_phys) >> 32;
+    uintptr_t task_state_addr = (uintptr_t)&task_state;
+    gdt[GDT_TSS >> 3] = (sizeof(TaskState) - 1) | ((task_state_addr & 0xffffff) << 16) | (9ul << 40) | (0ul << 45) |
+                        (1ul << 47) | (1ul << 53) | ((task_state_addr >> 24) << 56);
+    gdt[(GDT_TSS >> 3) + 1] = (task_state_addr) >> 32;
 
     // init gdt_desc
     gdt_desc.addr = (uintptr_t)gdt;
@@ -379,18 +283,6 @@ static Trapframe make_trapframe(uint64_t rip, uint64_t rsp) {
     tf.tf_rflags |= 0x200;                         // user & NO IOPL
     tf.tf_rsp = rsp;
     tf.tf_ss = GDT_USER_SS | 3;
-    return tf;
-}
-
-static Trapframe make_trapframe_32(uint64_t eip, uint64_t esp) {
-    Trapframe tf;
-    memset(&tf, 0, sizeof(tf));
-    tf.tf_rip = eip;
-    tf.tf_cs = GDT_USER32_CS | 3;
-    tf.tf_rflags = x86_64::read_rflags() & ~0x8d5; // clear all status bits
-    tf.tf_rflags |= 0x200;                         // user & NO IOPL
-    tf.tf_rsp = esp;
-    tf.tf_ss = GDT_USER32_SS | 3;
     return tf;
 }
 
@@ -426,33 +318,4 @@ void run_user_64(uint64_t entry, uint64_t rsp, uint64_t time_limit_ns, uint64_t 
     }
 }
 
-void run_user_32(uint64_t entry, uint64_t esp, uint64_t time_limit_ns, uint64_t &tsc, uint8_t &trap_num,
-                 int32_t &return_code) {
-    LDEBUG_ENTER_RET();
-
-    user_time_limit_ns = time_limit_ns;
-    tf_to_user = make_trapframe_32(entry, esp);
-
-    __asm__ volatile("int %1" : "=a"(trap_num) : "i"(TRAP_RUN_USER32) : "memory");
-
-    // export tsc
-    tsc = tf_run_user.tf_regs.tsc;
-
-    // export return-code
-    if (trap_num == TRAP_SYSCALL32) {
-        trap_num = TRAP_SYSCALL; // for API compatibility
-        int syscall_num = (int)(uint32_t)tf_from_user.tf_regs.rax;
-        if (syscall_num != ABI::DUCK_sys_exit_32) {
-            trap_num = TRAP_INVALID_SYSCALL;
-            return_code = 0;
-        } else {
-            return_code = (int32_t)(uint32_t)tf_from_user.tf_regs.rbx;
-        }
-    } else if (trap_num == TRAP_SYSCALL) {
-        trap_num = TRAP_INVALID_SYSCALL;
-        return_code = 0;
-    } else {
-        return_code = 0;
-    }
-}
 } // namespace Trap
